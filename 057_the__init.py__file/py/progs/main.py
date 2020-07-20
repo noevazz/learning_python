a08e7136354b54e2a619d195afe028abd9fc0924
noe vaz <jonovagu@gmail.com>
2020-07-19 20:58:29 -0500

    [ADD] packages

---
+++ 057_the__init.py__file/py/progs/main.py
@@ -0,0 +1,14 @@
+# main2.py
+
+from sys import path
+
+path.append('/home/noe/Documents/repos/learning_python/057_the__init.py__file/py/packages')
+
+import extra.iota
+from extra.good import alpha
+print(extra.iota.FunI())
+print(alpha.FunA())
+
+from extra import iota
+
+print(iota.FunI())
