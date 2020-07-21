a02b457687a579c73f9e12cf14a63c553c7ab64a
noe vaz <jonovagu@gmail.com>
2020-07-21 09:21:20 -0500

    [IMP] improving the explanation

--- 108.exceptions_are_classes.py
+++ 108.exceptions_are_classes.py
@@ -4,5 +4,13 @@ except Exception as e:
     print(e)
     print(e.__str__())
 
-
+"""
+without "trying" the code:
 i = int("Hello!")
+
+this will raise a ValueError exception
+ValueError: invalid literal for int() with base 10: 'Hello!'
+
+In "except Exception as e" e is equals to "invalid literal for int() with base 10: 'Hello!'"
+
+"""
